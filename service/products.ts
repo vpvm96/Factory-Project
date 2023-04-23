db7e5a3e20684c20c7bb22e80d9faf8039fcf290
vpvm96 <vpvm96@gmail.com>
2023-04-23 12:56:21 +0900

    feat: Product 해당 상품 한개만 가져오는 Service 추가

--- service/products.ts
+++ service/products.ts
@@ -10,10 +10,19 @@ export type Products = {
   date: string;
 };
 
+// export type Product = Products & {  }
+
+// 모든 상품의 데이터
 export async function getProductsService(): Promise<Products[]> {
   const filePath = path.join(process.cwd(), "data", "products.json");
 
   return readFile(filePath, "utf-8")
     .then<Products[]>(JSON.parse)
     .then((product) => product.sort((a, b) => (a.date > b.date ? -1 : 1)));
+}
+
+export async function getProductService(id: string) {
+  const metadata = await getProductsService();
+
+  return metadata.find((product) => product.id === Number(id));
 }
