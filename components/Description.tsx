52045dd760889aa47ca5d1f4e53a817cc041459e
vpvm96 <vpvm96@gmail.com>
2023-05-20 16:53:13 +0900

    fix: Proudect Detail 판매가 원산지란 추가

--- components/Description.tsx
+++ components/Description.tsx
@@ -9,10 +9,17 @@ export default function Description({ product }: Props) {
 
   return (
     <section className="w-1/3">
-      <div className="flex flex-col gap-3">
+      <div className="flex flex-col gap-2">
         <p className="text-lg text-gray-500">{category}</p>
         <h2 className="text-2xl font-bold">{title}</h2>
-        <p className="self-end text-xl font-bold text-sky-500">{price}</p>
+      </div>
+      <div className="mt-3 flex flex-row gap-12">
+        <p className="font-semibold text-slate-500">판매가</p>
+        <p className="text-l font-bold">{`${price.toLocaleString()}원`}</p>
+      </div>
+      <div className="mt-3 flex flex-row gap-12">
+        <p className="font-semibold text-slate-500">원산지</p>
+        <p className="text-l font-bold">멕시코</p>
       </div>
     </section>
   );
