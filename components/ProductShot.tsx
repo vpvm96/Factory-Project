083944a3be55e6eddcf5afdf969f7977b45cc047
vpvm96 <vpvm96@gmail.com>
2023-05-20 16:54:19 +0900

    fix: 상품 디테일페이지 상세보기칸 border-t & margin-top 적용

--- components/ProductShot.tsx
+++ components/ProductShot.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function ProductShot({ product: { image, title } }: Props) {
   return (
-    <section className="px-40">
+    <section className="mt-10 border-t border-gray-400 px-40">
       <h2 className=" my-8 text-center text-2xl font-bold">
         Product Detail Image
       </h2>
