7735790bb603f172f767083b60ec63e6d00f96fa
vpvm96 <vpvm96@gmail.com>
2023-06-27 21:51:54 +0900

    fix: 검색창 UI 일부 수정

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -7,16 +7,20 @@ export default function SearchInput() {
   const { searchValue, handleValueChange } = useSearch();
 
   return (
-    <div className="relative mx-auto flex justify-center text-gray-600">
+    <div className="relative w-full mx-auto flex justify-center text-gray-600">
       <input
-        className=" h-10 rounded-lg border-2 border-gray-300 bg-white px-5 pr-16 text-sm focus:outline-none"
-        type="search"
+        className="w-full pl-2 h-10 rounded-lg border-2 border-gray-300 bg-white text-sm focus:outline-none"
         name="search"
         placeholder="Search"
         value={searchValue}
         onChange={handleValueChange}
       />
-      <button type="submit" className="absolute right-9 top-0 mr-8 mt-3">
+      <button
+        type="submit"
+        className="absolute
+        right-0 top-0 mt-3 mr-2
+      "
+      >
         <CiSearch />
       </button>
     </div>
